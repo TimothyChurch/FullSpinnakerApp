94eb24848741edf7702a467e1baaf9e1f6d02c09
TimothyChurch <75554584+TimothyChurch@users.noreply.github.com>
2021-03-10 23:09:08 -0600

    Made about section Fluid

--- src/views/sections/About.vue
+++ src/views/sections/About.vue
@@ -3,8 +3,11 @@
     id="about"
     class="about"
   >
-    <v-container class="d-flex align-center fill-height px-4 py-12">
-      <v-row>
+    <v-container
+      fluid
+      class="px-4 py-12 mx-0"
+    >
+      <v-row align="center">
         <v-col cols="6">
           <v-img
             :src="require('@/assets/sailboat.jpg')"
@@ -19,7 +22,6 @@
           <base-body>
             There is a moment when you are sailing that you decide to fly the spinnaker. With the wind at your back, the massive sail unfurls and seems to effortlessly fill with air. Lifting you up and pulling you along, the full spinnaker helps your boat to seemingly fly accross the water! It is an amazing feeling!<br><br>This is what we want our owners to feel with our management. Our job is to do everythign we can to make sure your vacation rental performs at peak performance. We want you to be able to recline, relax, and let us help your vacation rental to fly!
           </base-body>
-          <base-contact />
         </v-col>
       </v-row>
     </v-container>
@@ -30,17 +32,6 @@
   export default {
     name: 'About',
 
-    provide: {
-      theme: { isDark: false },
-    },
-
-    computed: {
-      minHeight () {
-        const height = this.$vuetify.breakpoint.mdAndUp ? '100vh' : '50vh'
-
-        return `calc(${height} - ${this.$vuetify.application.top}px)`
-      },
-    },
   }
 </script>
 
